433cdd6737f6acb0e55f47e608edafa14ec5176e
Lynn Li <lynn.li@vechain.com>
2018-05-03 15:24:41 +0800

    test

--- console/console.js
+++ console/console.js
@@ -1,81 +1,87 @@
-(function() {
-  'use strict';
+;(function() {
+  'use strict'
 
-  hterm.defaultStorage = new lib.Storage.Local();
+  hterm.defaultStorage = new lib.Storage.Local()
 
-  var port;
+  var port
 
-  let textEncoder = new TextEncoder();
+  let textEncoder = new TextEncoder()
 
-  let t = new hterm.Terminal();
+  let t = new hterm.Terminal()
   t.onTerminalReady = () => {
-    console.log('Terminal ready.');
-    let io = t.io.push();
+    console.log('Terminal ready.')
+    let io = t.io.push()
 
     io.onVTKeystroke = str => {
       if (port !== undefined) {
         port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
+          t.io.println('Send error: ' + error)
+        })
       }
-    };
+    }
 
     io.sendString = str => {
       if (port !== undefined) {
         port.send(textEncoder.encode(str)).catch(error => {
-          t.io.println('Send error: ' + error);
-        });
+          t.io.println('Send error: ' + error)
+        })
       }
-    };
-  };
+    }
+  }
 
   document.addEventListener('DOMContentLoaded', event => {
-    let connectButton = document.querySelector('#connect');
+    let connectButton = document.querySelector('#connect')
 
-    t.decorate(document.querySelector('#terminal'));
-    t.setWidth(80);
-    t.setHeight(24);
-    t.installKeyboard();
+    t.decorate(document.querySelector('#terminal'))
+    t.setWidth(80)
+    t.setHeight(24)
+    t.installKeyboard()
 
     function connect() {
-      t.io.println('Connecting to ' + port.device_.productName + '...');
-      port.connect().then(() => {
-        console.log(port);
-        t.io.println('Connected.');
-        connectButton.textContent = 'Disconnect';
-        port.onReceive = data => {
-          let textDecoder = new TextDecoder();
-          t.io.print(textDecoder.decode(data));
+      t.io.println('Connecting to ' + port.device_.productName + '...')
+      port.connect().then(
+        () => {
+          console.log(port)
+          t.io.println('Connected.')
+          connectButton.textContent = 'Disconnect'
+          port.onReceive = data => {
+            let textDecoder = new TextDecoder()
+            t.io.print(textDecoder.decode(data))
+          }
+          port.onReceiveError = error => {
+            t.io.println('Receive error: ' + error)
+          }
+        },
+        error => {
+          t.io.println('Connection error: ' + error)
         }
-        port.onReceiveError = error => {
-          t.io.println('Receive error: ' + error);
-        };
-      }, error => {
-        t.io.println('Connection error: ' + error);
-      });
-    };
+      )
+    }
 
     connectButton.addEventListener('click', function() {
       if (port) {
-        port.disconnect();
-        connectButton.textContent = 'Connect';
+        port.disconnect()
+        connectButton.textContent = 'Connect'
       } else {
-        serial.requestPort().then(selectedPort => {
-          port = selectedPort;
-          connect();
-        }).catch(error => {
-          t.io.println('Connection error: ' + error);
-        });
+        serial
+          .requestPort()
+          .then(selectedPort => {
+            port = selectedPort
+            connect()
+          })
+          .catch(error => {
+            t.io.println('Connection error: ' + error)
+          })
       }
-    });
+    })
 
     serial.getPorts().then(ports => {
       if (ports.length == 0) {
-        t.io.println('No devices found.');
+        t.io.println('No devices found.')
       } else {
-        port = ports[0];
-        connect();
+        port = ports[0]
+        connect()
       }
-    });
-  });
-})();
+    })
+  })
+})()
