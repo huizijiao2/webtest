836adc405a110067fa32297a94d42f3c36432d9c
Lynn Li <lynn.li@vechain.com>
2018-05-03 14:35:41 +0800

    test

--- serial.js
+++ serial.js
@@ -1,13 +1,13 @@
-var serial = {};
+var serial = {}
 
-(function() {
-  'use strict';
+;(function() {
+  'use strict'
 
   serial.getPorts = function() {
     return navigator.usb.getDevices().then(devices => {
-      return devices.map(device => new serial.Port(device));
-    });
-  };
+      return devices.map(device => new serial.Port(device))
+    })
+  }
 
   serial.requestPort = function() {
     const filters = [
@@ -17,57 +17,70 @@ var serial = {};
       // { 'vendorId': 0x2341, 'productId': 0x804e },
       // { 'vendorId': 0x2341, 'productId': 0x804f },
       // { 'vendorId': 0x2341, 'productId': 0x8050 },
-      { 'vendorId': 0x0483, 'productId': 0x5710 }
-    ];
-    return navigator.usb.requestDevice({ 'filters': filters }).then(
-      device => new serial.Port(device)
-    );
+      { vendorId: 0x0483, productId: 0x5710 }
+    ]
+
+    const openPort = {
+      requestType: 'standard',
+      recipient: 'device',
+      request: 0x06,
+      value: 0x01,
+      index: 0x00
+    }
+
+    return navigator.usb
+      .requestDevice({ filters: filters })
+      .then(device => new serial.Port(device))
   }
 
   serial.Port = function(device) {
-    this.device_ = device;
-  };
+    this.device_ = device
+  }
 
   serial.Port.prototype.connect = function() {
     let readLoop = () => {
-      this.device_.transferIn(5, 64).then(result => {
-        this.onReceive(result.data);
-        readLoop();
-      }, error => {
-        this.onReceiveError(error);
-      });
-    };
+      this.device_.transferIn(5, 64).then(
+        result => {
+          this.onReceive(result.data)
+          readLoop()
+        },
+        error => {
+          this.onReceiveError(error)
+        }
+      )
+    }
 
-    return this.device_.open()
-        .then(() => {
-          if (this.device_.configuration === null) {
-            return this.device_.selectConfiguration(1);
-          }
-        })
-        .then(() => this.device_.claimInterface(0))
-        .then(() => this.device_.selectAlternateInterface(0, 0))
-        .then(() => this.device_.controlTransferOut({
-            'requestType': 'class',
-            'recipient': 'interface',
-            'request': 0x22,
-            'value': 0x01,
-            'index': 0x00}))
-        .then(() => {
-          readLoop();
-        });
-  };
+    return this.device_
+      .open()
+      .then(() => {
+        if (this.device_.configuration === null) {
+          return this.device_.selectConfiguration(1)
+        }
+      })
+      .then(() => this.device_.claimInterface(0))
+      .then(() => this.device_.selectAlternateInterface(0, 0))
+      .then(() =>
+        this.device_.controlTransferOut(openPort)
+      )
+      .then(() => {
+        console.log('in')
+        // readLoop()
+      })
+  }
 
   serial.Port.prototype.disconnect = function() {
-    return this.device_.controlTransferOut({
-            'requestType': 'class',
-            'recipient': 'interface',
-            'request': 0x22,
-            'value': 0x00,
-            'index': 0x00})
-        .then(() => this.device_.close());
-  };
+    return this.device_
+      .controlTransferOut({
+        requestType: 'class',
+        recipient: 'interface',
+        request: 0x22,
+        value: 0x00,
+        index: 0x00
+      })
+      .then(() => this.device_.close())
+  }
 
   serial.Port.prototype.send = function(data) {
-    return this.device_.transferOut(4, data);
-  };
-})();
+    return this.device_.transferOut(4, data)
+  }
+})()
